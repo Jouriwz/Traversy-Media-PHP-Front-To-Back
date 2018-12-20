003ca378d7b399da0b025019f643569b2408ec61
Jouri Zevenhek <Jouri.w.z@gmail.com>
2018-12-20 21:47:24 +0100

    Finished Shorthand syntax

--- shorthands.php
+++ shorthands.php
@@ -1,6 +1,7 @@
 <?php
     $loggedIn = false;
-
+    $arr = [1,2,3,4,5];
+    
     // if($loggedIn){
     //     echo 'You are logged in';
     // } else {
@@ -12,7 +13,37 @@
     $isRegistered = ($loggedIn == true) ? true : false;
     echo $isRegistered;
 
-    $age = 20;
-    $score = 15;
+    $age = 7;
+    $score = 12;
 
-    echo 'Your score is: '.($score > 10 ? ($age > 10 ? 'Average': 'Exceptional'):($age > 10 ? 'Horrible':'Average'));
+    echo 'Your score is: '.($score > 10 ? ($age > 10 ? 'Average': 'Exceptional'):($age > 10 ? 'Horrible':'Average')), PHP_EOL;
+
+?>
+
+<div>
+<?php if($loggedIn){ ?>
+    <h1>Welcome User</h1>
+<?php } else { ?>
+    <h1>Welcome Guest</h1>
+<?php } ?>
+</div>
+
+<div>
+<?php if($loggedIn): ?>
+    <h1>Welcome User</h1>
+<?php else: ?>
+    <h1>Welcome Guest</h1>
+<?php endif; ?>
+</div>
+
+<div>
+<?php foreach($arr as $val): ?>
+    <?php echo $val; ?>
+<?php endforeach; ?>
+</div>
+
+<div>
+<?php for($i = 0;i < 10;$i++): ?>
+    <li><?php echo $val; ?></li>
+<?php endfor; ?>
+</div>
